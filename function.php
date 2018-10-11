7bf722222eb8eeac47774102600d270d7a672e0f
kibriakk <37658601+kibriakk@users.noreply.github.com>
2018-10-11 11:00:03 +0600

    Update function.php

--- function.php
+++ function.php
@@ -1,19 +1,13 @@
 <?php
-$baseurl = "https://domain.com";
-$sender = "no-reply@domain.com";
-
-	error_reporting(E_ALL);
-	
-$dbname = "lotto_lotto";
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-
-
-
-$basecurrency = "USD";
-
-
+$baseurl = "https://domain.com";      //input base url
+$sender = "no-reply@domain.com";  //input sender email adress
+    error_reporting(E_ALL);
+    
+$dbname = "lotto_lotto";   //input database name
+$dbhost = "localhost";   //input database host
+$dbuser = "root";   //input database username
+$dbpass = "";   //input database password
+$basecurrency = "USD";    //change USD to your base currency
 function connectdb()
 {
     global $dbname, $dbuser, $dbhost, $dbpass;
@@ -22,53 +16,40 @@ function connectdb()
     $condb = @mysql_select_db($dbname);
     if(!$condb) return false;
     return true;
-}
-
-
-
+}   //database connection function
 function is_user()
 {
-	if (isset($_SESSION['username']))
-		return true;
-}
-
+    if (isset($_SESSION['username']))
+        return true;
+}   
 function redirect($url)
 {echo "<meta http-equiv=\"refresh\" content=\"0; url=$url\" />";
-	//header('Location: ' .$url);
-	exit;
+    //header('Location: ' .$url);
+    exit;
 }
-
 function valid_username($str){
-	return preg_match('/^[a-z0-9_-]{3,16}$/', $str);
+    return preg_match('/^[a-z0-9_-]{3,16}$/', $str);
 }
-
 function valid_password($str){
-	return preg_match('/^[a-z0-9_-]{6,18}$/', $str);
+    return preg_match('/^[a-z0-9_-]{6,18}$/', $str);
 }
-
-
-
-
 //////////////////GENERATE TRX #
-$a1 = date("ymd", time());
-$a2 = rand(100,999);
-$u = substr(uniqid(), 7);
-$c = chr(rand(97,122));
-$c2 = chr(rand(97,122));
-$c3 = chr(rand(97,122));
-$ok = "$c$u$c2$a2$c3";
+$trxd = date("ymd", time());
+$trxr = rand(100,999);
+$trxu = substr(uniqid(), 7);
+$trxc1 = chr(rand(97,122));
+$trxc2 = chr(rand(97,122));
+$trxc3 = chr(rand(97,122));
+$ok = "$trxc1$trxd$trxu$trxc3$trxr";
 $txn_id = strtoupper($ok);
 //////////////////GENERATE TRX #
-
-
 function toBTC($usd){
 $api = "https://blockchain.info/tobtc?currency=USD&value=$usd";
 $BTC = file_get_contents($api);
 return $BTC;
-}
-
+}  //usd to btc converter
 function toScan($usd, $account){
 $var = "bitcoin:$account?amount=$usd";
 echo "<img src=\"https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=$var&choe=UTF-8\" title='' style='width:300px;' />";
-}
+}   //btc adress to QR code
 ?>
